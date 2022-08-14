b20892b1eb8a3c803bc17067a1ad097f40974ee4
Shayan Raza <shayanraza@gmail.com>
2022-08-14 18:49:55 +0300

    install script options

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "This script installs the config files of Shayan Raza"
+echo
+
+echo "Choose the config you want to install"
+echo "1) Neo Vim"
+echo "2) Alacritty"
+echo "3) All"
+echo
+
+read -p "Enter the number of the config you want to install: " selected
+
