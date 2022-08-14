a744647c0c76635744b825d2ea2052e68fbf7b1f
Shayan Raza <shayanraza@gmail.com>
2022-08-14 21:27:29 +0300

    alacritty install script

--- install.sh
+++ install.sh
@@ -13,13 +13,22 @@ read -p "Enter the number of the config you want to install: " selected
 
 # Functions for configs
 neovim () { 
+	echo "Chosen NeoVim"
 	sudo pacman -S neovim
 	mv ./nvim/ ~/.config
+}
+alacritty () { 
+	echo "Chosen Alacritty"
+	sudo pacman -S alacritty
+	mv ./alacritty ~/.config
 }
 
 # Running functions based on user input
 if [ $selected == 1 ] 
-then 
+then
 	neovim
+elif [ $selected == 2 ]  
+then
+	alacritty
 fi
 
