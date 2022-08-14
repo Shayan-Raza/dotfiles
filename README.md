9777af1e21d7de720ad254b8fadbc790d61d0e5b
Shayan Raza <shayanraza07@gmail.com>
2022-08-14 23:14:14 +0300

    Install Script (#2)
    * install script options
    
    * neovim install script
    
    * alacritty install script
    
    * rename alerts
    
    * README.md install script
    
    * font installer script

--- README.md
+++ README.md
@@ -2,22 +2,31 @@
 
 # Table of Contents
 - [Installation](#Installation)
+	- [Install Script](#Install-Script)
 	- [Fonts](#Fonts)
-	- [Programs](#Programs)
-- [Neo Vim](#nvim) 
-- [alacritty](#alacritty)
+- [Programs](#Programs)
+	- [Neo Vim](#nvim) 
+	- [alacritty](#alacritty)
 
 # Installation
+### Install-Script
+This repo also comes with an install script that supports installing the programs and moving the configs.
+```
+chmod +x install.sh
+```
+```
+./install.sh
+```
 ### Fonts
 - [JetBrains Mono Nerd Font](https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/JetBrainsMono/Ligatures)
 
-### Programs
+# Programs
 - [Neo Vim](http://neovim.io/)
 - [Alacritty](https://github.com/alacritty/alacritty)
 - [Vim Plug](https://github.com/junegunn/vim-plug)
 
-# Neo Vim
+### Neo Vim
 ![nvim screenshot](assets/screenshots/nvim.png)
 
-# alacritty
+### alacritty
 ![alacritty screenshot](assets/screenshots/alacritty.png)
---
+++ install.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+echo "This script installs the config files of Shayan Raza"
+echo
+
+echo "Choose the config you want to install"
+echo "1) Neo Vim"
+echo "2) Alacritty"
+echo "3) All"
+echo
+
+read -p "Enter the number of the config you want to install: " selected
+
+# Functions for configs
+neovim () { 
+	echo "Neovim Config"
+	sudo pacman -S neovim
+	mv ./nvim/ ~/.config
+}
+alacritty () { 
+	echo "Alacritty Config"
+	sudo pacman -S alacritty
+	mv ./alacritty ~/.config
+}
+
+# Running functions based on user input
+if [ $selected == 1 ] 
+then
+	neovim
+elif [ $selected == 2 ]  
+then
+	alacritty
+elif [ $selected == 3 ] 
+then 
+	echo "Chosen All"
+	neovim 
+	alacritty
+else
+	echo "Number not found"
+	exit
+fi
+
+#Font Install
+while true; do
+	read -p "Do you want to install the JetBrains Mono Font (Y/n): " yn
+    case $yn in
+        [Yy]* ) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)" ;
+                break;;
+        [Nn]* ) echo "You choose not to install the font." ;
+                break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
