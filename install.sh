f44df998e33d8302fe5336dc20245385e9fd9352
Shayan Raza <shayanraza@gmail.com>
2022-08-15 17:24:22 +0300

    vim plug install script

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ read -p "Enter the number of the config you want to install: " selected
 neovim () { 
 	echo "Neovim Config"
 	sudo pacman -S neovim
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	mv ./nvim/ ~/.config
 }
 alacritty () { 
